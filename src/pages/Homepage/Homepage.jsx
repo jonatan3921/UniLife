01a9f776ba9c5a33fb19e1516ed1d4fecf39f16a
jonatan3921 <jonatan3921@gmail.com>
2023-07-21 13:07:16 -0400

    Cover Done

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Search from '../../components/Search/Search'
 import './Homepage.css'
 
 function Homepage() {
   return (
     <div className='homepage-container'>
-        
+        <div className='cover-container'>
+            <h1>Find student homes <br/> with bills included</h1>
+            <p>A simple and faster way to search for student accommodation</p>
+            <Search />
+        </div>
     </div>
   )
 }
