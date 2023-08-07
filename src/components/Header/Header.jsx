2fc7e77c79f405b16abf2e1807474ba120b57d57
jonatan3921 <jonatan3921@gmail.com>
2023-08-06 22:28:08 -0400

    hamburger menu done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -27,56 +27,69 @@ Modal.setAppElement(document.getElementById('root'));
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false)
+  const [isActived, setIsActived] = useState(false);
 
+
+  const handleMenu = () => {
+    setIsActived(!isActived);
+  }
 
   return (
     <div className='header-container'>
-      <div className='logo-container'>
+      <div className={isActived ? 'no-show' : 'logo-container'}>
         <BsHouses />
         <Link to={'/'}><h3>UniLife</h3></Link>
       </div>
-      <div className='other-links'>
-        <div className='shortlist-container'>
-          <AiOutlineHeart />
-          <Link to={'/'}><p>Shortlist</p></Link>
-        </div>
-        <div className='contactus-container'>
-          <AiOutlineMail />
-          <button onClick={() => setIsOpen(true)}>Contact Us</button>
-          <Modal
-          isOpen={isOpen}
-          onRequestClose={() => setIsOpen(false)}
-          style={customStyles}
-          contentLabel="Contact Us Modal"
-          >
-              <div className='modal-header-contactus'>
-                <h2>Contact Us</h2>
-                <AiOutlineMail className='mail-icon'/>
-                </div>
-                <p className='contactus-text'>Feel free to contact us if you have any questions.
-Looking forward to hear from you.</p>
-                <form className='modal-form-contactus'>
-                <div>
-                    <label htmlFor="name">Name</label>
-                    <input type="text" id="name" placeholder='Enter your name'/>
-                    <label htmlFor='phoneNumber'>Phone Number</label>
-                    <input type='number' id='phoneNumber' placeholder='Enter your phone number'/>
-                    <label htmlFor='select'>Are you a...</label>
-                    <select id='select'>
-                      <option value='Student'>Student</option>
-                      <option value='Professor'>Professor</option>
-                    </select>
-                </div>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" placeholder='Enter your email address'/>
-                    <label htmlFor="message">Message</label>
-                    <textarea id="message" rows="4" placeholder='Enter your message'></textarea>
-                    <button type="submit">Submit</button>
-                </div>
-                </form>
-          </Modal>
-        </div>
+      <div className={isActived ? 'other-links other-links-actived' : 'other-links'}>
+        <label className='burger' onClick={handleMenu}>{
+          isActived ?
+          <p>x</p>
+          :
+          <p>&#9776;</p>
+        }</label>
+        <nav className={isActived ? 'show-menu' : 'no-show'}>
+          <div className='shortlist-container'>
+            <AiOutlineHeart />
+            <Link to={'/'}><p>Shortlist</p></Link>
+          </div>
+          <div className='contactus-container'>
+            <AiOutlineMail />
+            <button onClick={() => setIsOpen(true)}>Contact Us</button>
+            <Modal
+            isOpen={isOpen}
+            onRequestClose={() => setIsOpen(false)}
+            style={customStyles}
+            contentLabel="Contact Us Modal"
+            >
+                <div className='modal-header-contactus'>
+                  <h2>Contact Us</h2>
+                  <AiOutlineMail className='mail-icon'/>
+                  </div>
+                  <p className='contactus-text'>Feel free to contact us if you have any questions.
+  Looking forward to hear from you.</p>
+                  <form className='modal-form-contactus'>
+                  <div>
+                      <label htmlFor="name">Name</label>
+                      <input type="text" id="name" placeholder='Enter your name'/>
+                      <label htmlFor='phoneNumber'>Phone Number</label>
+                      <input type='text' id='phoneNumber' placeholder='Enter your phone number'/>
+                      <label htmlFor='select'>Are you a...</label>
+                      <select id='select'>
+                        <option value='Student'>Student</option>
+                        <option value='Professor'>Professor</option>
+                      </select>
+                  </div>
+                  <div>
+                      <label htmlFor="email">Email</label>
+                      <input type="email" id="email" placeholder='Enter your email address'/>
+                      <label htmlFor="message">Message</label>
+                      <textarea id="message" rows="4" placeholder='Enter your message'></textarea>
+                      <button type="submit">Submit</button>
+                  </div>
+                  </form>
+            </Modal>
+          </div>
+        </nav>
       </div>
     </div>
   )
