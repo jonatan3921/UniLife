10f44f0f1abf3cd6da6fef5cc77b704dcd7f2598
jonatan3921 <jonatan3921@gmail.com>
2023-08-10 11:37:37 -0400

    Project done

--- src/components/BedroomPrices/BedroomPrices.jsx
+++ src/components/BedroomPrices/BedroomPrices.jsx
@@ -4,12 +4,25 @@ import './BedroomPrices.css'
 function BedroomPrices({home}) {
 
     const bedroomPriceObject = home?.bedroom_prices;
+    let bedroomLength;
+    let bedroomNumber = [];
+    if(bedroomPriceObject !== undefined) {
+            bedroomLength = Object.keys(bedroomPriceObject).length;
+            for (let i = 0; i < bedroomLength; i++) {
+                bedroomNumber.push(`Bedroom ${i + 1}`)
+            }
+            console.log(bedroomNumber);
+    }
+    
+    
     
     let displayBedroomAndPrice;
     if (bedroomPriceObject !== undefined) {
         displayBedroomAndPrice = Object.keys(bedroomPriceObject).map(key => (
         <div className='bedroom-and-price' key={key}>
-            <p className='bedroomNumber'>{key}:</p>
+            <p className='bedroomNumber'>{
+            bedroomNumber[Object.keys(bedroomPriceObject).indexOf(key)]}:
+            </p>
             <p className='bedroomPrice'>£{bedroomPriceObject[key]} per week</p>
         </div>
     ))
