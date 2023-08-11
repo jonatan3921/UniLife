f01ee80416896a8d5e11cc1eee21c8f18bcee7db
jonatan3921 <jonatan3921@gmail.com>
2023-08-11 13:23:23 -0400

    Done

--- src/components/BedroomPrices/BedroomPrices.jsx
+++ src/components/BedroomPrices/BedroomPrices.jsx
@@ -2,26 +2,16 @@ import React from 'react'
 import './BedroomPrices.css'
 
 function BedroomPrices({home}) {
-
     const bedroomPriceObject = home?.bedroom_prices;
-    let bedroomLength;
-    let bedroomNumber = [];
-    if(bedroomPriceObject !== undefined) {
-            bedroomLength = Object.keys(bedroomPriceObject).length;
-            for (let i = 0; i < bedroomLength; i++) {
-                bedroomNumber.push(`Bedroom ${i + 1}`)
-            }
-            console.log(bedroomNumber);
-    }
     
     
     
     let displayBedroomAndPrice;
     if (bedroomPriceObject !== undefined) {
-        displayBedroomAndPrice = Object.keys(bedroomPriceObject).map(key => (
+        displayBedroomAndPrice = Object.keys(bedroomPriceObject).map((key, index) => (
         <div className='bedroom-and-price' key={key}>
-            <p className='bedroomNumber'>{
-            bedroomNumber[Object.keys(bedroomPriceObject).indexOf(key)]}:
+            <p className='bedroomNumber'>
+            Bedroom {index + 1}:
             </p>
             <p className='bedroomPrice'>£{bedroomPriceObject[key]} per week</p>
         </div>
